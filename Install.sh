9093c4db02d79dcd31ca2f10ad8096b9f2b6f7e4
kbvstmd <kbvstmd@gmail.com>
2021-05-31 15:30:35 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -24,11 +24,11 @@
 #  wget http://119.3.41.228:8080/XCNV/data/hg19_ljb26_all_converted.vcf.gz -O ./data/hg19_ljb26_all_converted.vcf.gz
 #  gunzip ./data/hg19_ljb26_all_converted.vcf.gz
 #fi
-wget http://122.112.206.160/XCNV/data/hg19_ljb26_all_converted_scores.txt -O ./data/hg19_ljb26_all_converted_scores.txt
-wget http://122.112.206.160/XCNV/data/hg19_ljb26_all_converted_sites.vcf -O ./data/hg19_ljb26_all_converted_sites.vcf
-wget http://122.112.206.160/XCNV/data/CDTS_percentile.txt -O ./data/CDTS_percentile.txt
-wget http://122.112.206.160/XCNV/data/merged.cnv.sample.info.txt -O ./data/merged.cnv.sample.info.txt
- wget http://122.112.206.160/XCNV/data/merged.cnv.data.output.csv -O ./data/merged.cnv.data.output.csv
+wget http://119.3.41.228/XCNV/data/hg19_ljb26_all_converted_scores.txt -O ./data/hg19_ljb26_all_converted_scores.txt
+wget http://119.3.41.228/XCNV/data/hg19_ljb26_all_converted_sites.vcf -O ./data/hg19_ljb26_all_converted_sites.vcf
+wget http://119.3.41.228/XCNV/data/CDTS_percentile.txt -O ./data/CDTS_percentile.txt
+wget http://119.3.41.228/XCNV/data/merged.cnv.sample.info.txt -O ./data/merged.cnv.sample.info.txt
+wget http://119.3.41.228/XCNV/data/merged.cnv.data.output.csv -O ./data/merged.cnv.data.output.csv
 
 rscript=`which Rscript`
 script_path=`pwd`
